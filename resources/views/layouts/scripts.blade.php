a6e469d63e13d2b9c7ddc7df28941bd46d15473f
LaurentiuCalin <durnea.calin@gmail.com>
2017-10-13 09:52:20 +0200

    added sweet alert 2

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,5 +1,18 @@
 <!-- Scripts -->
+{{--jquery--}}
+<script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+
 <script src="{{ asset('js/app.js') }}"></script>
+
+{{--sweetalert--}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"
         integrity="sha256-egVvxkq6UBCQyKzRBrDHu8miZ5FOaVrjSqQqauKglKc=" crossorigin="anonymous"></script>
+
+{{--sweetalert2--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.1/sweetalert2.all.min.js"
+        integrity="sha256-Cx9rA5vmyLN1w4VBrMl1cCaCD5FN7K+H1uTpf0/V+7s=" crossorigin="anonymous"></script>
+
 @include('sweet::alert')
