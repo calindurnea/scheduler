77674058262045f5800fd5f671972e0939fe033c
LaurentiuCalin <durnea.calin@gmail.com>
2017-10-13 09:45:25 +0200

    access users list and logged user from anywhere

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController {
 
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+	/**
+	 * @var type
+	 */
+	protected $user, $space;
+
 	public function __construct() {
 		$this->middleware('auth');
+		$this->middleware(function($request, $next) {
+			$this->user = request()->user();
+
+			return $next($request);
+		});
+
+		$users = User::withRoles(['member', 'temp', 'manager'])->get();
+		View::share('users', $users);
 	}
 }
