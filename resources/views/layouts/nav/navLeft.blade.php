19bc8766311fe11f10309d05eef8f41064e94124
LaurentiuCalin <durnea.calin@gmail.com>
2017-10-13 09:49:36 +0200

    removed due to bootstrap 4

--- resources/views/layouts/nav/navLeft.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Left Side Of Navbar -->
-<ul class="nav navbar-nav">
-    @guest
-        @else
-            <li><a href="{{route('schedule_show')}}">Schedule</a></li>
-            <li><a href="{{route('users_show')}}">Users</a></li>
-            <li><a href="{{route('user_create')}}">Add user</a></li>
-            @endguest
-</ul>
