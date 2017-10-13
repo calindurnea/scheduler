b5bdd4d8995363ffc3a3638d918f5c3e44c6a08c
LaurentiuCalin <durnea.calin@gmail.com>
2017-10-13 09:48:34 +0200

    edit user ajax call - unfinished

---
+++ resources/views/users/script.blade.php
@@ -0,0 +1,29 @@
+<script>
+
+    {{--//edit modal--}}
+    {{--$('.btn-edit').click(function () {--}}
+        {{--dataToEdit = $(this).data('edit');--}}
+
+        {{--if (dataToEdit === 'schedule') {--}}
+            {{--routeUrl = 'schedule/edit';--}}
+        {{--} else {--}}
+            {{--id = '{{$user->id}}';--}}
+            {{--routeUrl = 'user/edit';--}}
+        {{--}--}}
+
+        {{--$.ajax({--}}
+                {{--url: routeUrl,--}}
+                {{--data: {id: id},--}}
+                {{--dataType: 'json',--}}
+                {{--type: 'POST',--}}
+                {{--success: function (data) {--}}
+                    {{--console.log(data);--}}
+                {{--},--}}
+                {{--error: function (error) {--}}
+                    {{--console.log(error);--}}
+                {{--}--}}
+            {{--}--}}
+        {{--)--}}
+        {{--// $('#editModal').modal('show');--}}
+    {{--});--}}
+</script>
