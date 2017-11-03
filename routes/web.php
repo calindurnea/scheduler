6ea4d61ac263cc8e08c043ca13f2f54a610b7a59
LaurentiuCalin <durnea.calin@gmail.com>
2017-11-03 12:42:51 +0100

    changed names and added colors routes

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,10 @@ Route::post('/user', ['as' => 'users_show', 'uses' => 'UserController@show']);
 
 Route::get('/users/create', ['as' => 'users_create', 'uses' => 'UserController@create']);
 Route::post('/users/store', ['as' => 'users_store', 'uses' => 'UserController@store']);
-//Route::post('/user/edit', ['as' => 'user_edit', 'uses' => 'UserController@edit']);
-//Route::post('/user/update', ['as' => 'user_update', 'uses' => 'UserController@update']);
+//Route::post('/users/edit', ['as' => 'user_edit', 'uses' => 'UserController@edit']);
+//Route::post('/users/update', ['as' => 'user_update', 'uses' => 'UserController@update']);
+Route::post('/users/delete', ['as' => 'users_delete', 'uses' => 'UserController@destroy']);
+
 
 Route::get('/shifts', ['as' => 'shifts_get', 'uses' => 'ShiftController@index']);
 Route::post('/shifts/show', ['as' => 'shifts_show', 'uses' => 'ShiftController@show']);
@@ -33,3 +35,7 @@ Route::get('/schedule', ['as' => 'schedule_get', 'uses' => 'ScheduleController@i
 
 //Route::get('/schedule/edit', ['as' => 'schedule_edit', 'uses' => 'ScheduleController@edit']);
 //Route::post('/schedule/update', ['as' => 'schedule_update', 'uses' => 'ScheduleController@update']);
+
+Route::get('/colors', ['as' => 'colors_get', 'uses' => 'ColorController@index']);
+Route::post('/colors/store', ['as'=>'colors_store', 'uses'=>'ColorController@store']);
+Route::post('/colors/delete', ['as'=>'colors_delete', 'uses'=>'ColorController@destroy']);
