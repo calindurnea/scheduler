386556064bbc9bd8a9b1376cae8b79224d7399cb
Laurentiu Calin Durnea <durnea.calin@gmail.com>
2018-01-14 17:50:25 +0100

    fix

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -7,10 +7,6 @@
 
 <script src="{{ asset('js/app.js') }}"></script>
 
-{{--sweetalert--}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"
-        integrity="sha256-egVvxkq6UBCQyKzRBrDHu8miZ5FOaVrjSqQqauKglKc=" crossorigin="anonymous"></script>
-
 {{--sweetalert2--}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.1/sweetalert2.all.min.js"
         integrity="sha256-Cx9rA5vmyLN1w4VBrMl1cCaCD5FN7K+H1uTpf0/V+7s=" crossorigin="anonymous"></script>
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -2,10 +2,6 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-{{--sweetalert--}}
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css"
-      integrity="sha256-iXUYfkbVl5itd4bAkFH5mjMEN5ld9t3OHvXX3IU8UxU=" crossorigin="anonymous"/>
-
 {{--sweetalert2--}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.1/sweetalert2.min.css"
       integrity="sha256-dFCwuhCfoeoBeMuViN7hhbZEZW7takATo0uQ8SzZSI8=" crossorigin="anonymous"/>
