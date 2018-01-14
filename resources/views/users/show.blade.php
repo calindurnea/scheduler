4bc5586c12ec08b24cfa7d46c5a52f48d956e310
Laurentiu Calin Durnea <durnea.calin@gmail.com>
2018-01-14 15:50:53 +0100

    reorganized

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('components.backButton')
-    {{--<form action="{{route('user_edit', ['id'=>$user->id])}}" method="post">--}}
-    {{--{{csrf_field()}}--}}
-    {{--<input type="submit" class="btn btn-warning" value="Edit details">--}}
-    {{--</form>--}}
+    @include('components.backButton', ['route'=>route('users.index')])
     <div class="user_details_block">
         <h3><strong>{{$user->first_name}} {{$user->last_name}}</strong> <span
                     class="user_span_position">{{$user->roles()->pluck('role')->first()}}</span></h3>
@@ -26,38 +22,39 @@
 
     </div>
     @managerOrAdmin
-    <div class="dropdown">
-        <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-            Edit
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-            <button class="dropdown-item btn-edit" data-edit="name" type="button">Name
-            </button>
-            <button class="dropdown-item btn-edit" data-edit="email" type="button">Email
-            </button>
-            <button class="dropdown-item btn-edit" data-edit="phone" type="button">Phone
-            </button>
-            <button class="dropdown-item btn-edit" data-edit="color_id" type="button">Color
-            </button>
-            <button class="dropdown-item btn-edit" data-edit="position" type="button">Position
-            </button>
-            {{--<button class="dropdown-item btn-edit" data-edit="schedule" type="button">Schedule--}}
-            {{--</button>--}}
-        </div>
+    <br>
+    <div class="col-md-10">
+        <a href="{{route('users.edit', $user->id)}}">
+            <button class="btn btn-warning">Change information</button>
+        </a>
+        <button class="btn btn-danger" id="delete-user">Delete employee</button>
     </div>
-    <button class="btn btn-danger" id="delete-user">Delete employee</button>
     @endmanagerOrAdmin
-
     @include('schedule.editModal')
-    <ul>
+    <div class="col-md-6 container">
+        <br>
 
-        @foreach ($user->shifts as $key=>$shift)
-            <li>
-                {{$key}} => {{$shift}}
-            </li>
-        @endforeach
-    </ul>
+        <table data-toggle="table" class="table table-hover table-responsive-md" id="shifts-table"
+               data-pagination="true"
+               data-page-list="[5, 10, 20, 50, 100, 200]"
+               data-click-to-select="true">
+            <thead class="thead-inverse">
+            <tr>
+                <th data-formatter="runningFormatter" class="col-xs-1">Index</th>
+                <th scope="col" data-field="start" data-sortable="true" class="col-xs-5">Start</th>
+                <th scope="col" data-field="end" data-sortable="true" class="col-xs-5">End</th>
+                <th scope="col" data-field="duration" data-sortable="true" class="col-xs-1">Duration (hours)</th>
+                <th data-field="state" data-checkbox="true"></th>
+            </tr>
+            </thead>
+            <input type="text" name="shiftrange" class="col-md-3 pull-right">
+        </table>
+        @managerOrAdmin
+        <button class="btn btn-primary float-right" data-user-email="{{$user->email}}" id="mail-shifts">Mail
+            selected shifts
+        </button>
+        @endmanagerOrAdmin
+    </div>
 @endsection
 
 @section('pageScripts')
