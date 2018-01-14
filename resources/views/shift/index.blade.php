ac3b6f41fef2ccc0784c5b3ae855d262e82f76a0
Laurentiu Calin Durnea <durnea.calin@gmail.com>
2018-01-14 15:52:57 +0100

    added error template

--- resources/views/shift/index.blade.php
+++ resources/views/shift/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
+    @if(\Session::has('error'))
+        <div class="alert alert-danger col-md-2 pull-right">
+            {{\Session::get('error')}}
+        </div>
+    @endif
     <div id='calendar'></div>
 @endsection
 
